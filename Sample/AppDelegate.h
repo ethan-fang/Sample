15935c31f2bc58a572b47889927783bf184a8466
Ethan Fang <ethan.fang@hotmail.com>
2015-09-25 13:38:20 -0700

    first commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Ethan Fang on 9/25/15.
+//  Copyright © 2015 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Ethan Fang on 9/25/15.
+//  Copyright © 2015 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
