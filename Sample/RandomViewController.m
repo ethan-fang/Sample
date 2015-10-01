e8b21bc21317be54281e4f1b5eba62dc23efa52f
Ethan Fang <ethan.fang@hotmail.com>
2015-10-01 10:20:01 -0400

    Clean up

--- Sample/RandomViewController.m
+++ Sample/RandomViewController.m
@@ -18,36 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    self.i = 1;
-    // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    self.i = 2;
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-    
-    self.i = 3;
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Sample/ViewController.m
+++ Sample/ViewController.m
@@ -17,33 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    int i = 99;
-    BOOL even0 = [self isEven:i + 2];
-    BOOL even1 = [self isEven:i + 11];
-    
-
-    
-    NSLog(@"even0 %d even1 %d", even0, even1);
 }
 
 - (IBAction)pushButtonTapped:(id)sender {
     RandomViewController *randonVC = [[RandomViewController alloc] initWithNibName:NSStringFromClass([RandomViewController class]) bundle:nil];
     [self.navigationController pushViewController:randonVC animated:YES];
-}
-
-- (BOOL)isEven:(int)i {
-    if (i % 2 == 0) {
-        NSLog(@"%d is even", i);
-        return YES;
-    }
-    NSLog(@"%d is odd", i);
-    return NO;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
